468649a6f10c293ca24518d6d2cb04b3d9feff0c
Dennis Thisner <dennis@applydigital.co>
2021-05-26 15:46:59 -0700

    added some unit tests

--- main.go
+++ main.go
@@ -8,113 +8,108 @@ import (
 	"os"
 )
 
-type fileTransferStatus struct {
-        errorMsg string
-        customMsg string
-}
-
 func main() {
 	err := readPlaylist()
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func readPlaylist() error {
-    file, err := os.Open("/Users/dennis/Desktop/lovedTracks.m3u")
-    if err != nil {
-        return fmt.Errorf("Problem opening the playlist: '%s' \n", err)
-    }
-    defer file.Close()
+	file, err := os.Open("/Users/dennis/Desktop/lovedTracks.m3u")
+	if err != nil {
+		return fmt.Errorf("problem opening the playlist: '%s' \n ", err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    buf := make([]byte, 0, 64*1024) // Buffering size 1MB
-    scanner.Buffer(buf, 1024*1024)
+	scanner := bufio.NewScanner(file)
+	buf := make([]byte, 0, 64*1024) // Buffering size 1MB
+	scanner.Buffer(buf, 1024*1024)
 
-    // Looping through the playlist 
-    for scanner.Scan() {
-        orgLocation := scanner.Text()
-        if orgLocation[0:1] == "/" {
+	// Looping through the playlist
+	for scanner.Scan() {
+		orgLocation := scanner.Text()
+		if orgLocation[0:1] == "/" {
 
-            err = setupPath(fileDest(orgLocation))
-            if err != nil {
-                return err
-            }
-            err = copyFile(fileDest(orgLocation), orgLocation)
-            if err != nil {
-                return err
-            }
-        }
-    }
+			err = setupPath(fileDest(orgLocation))
+			if err != nil {
+				return err
+			}
+			err = copyFile(fileDest(orgLocation), orgLocation)
+			if err != nil {
+				return err
+			}
+		}
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 
-    return nil
+	return nil
 }
 
 func setupPath(fileDest string) error {
-    // Creating folder path if it dosn't exist
-    _, err := os.Stat(getFolderPath(fileDest))
-    if os.IsNotExist(err) {
-        err = os.MkdirAll(getFolderPath(fileDest), os.ModePerm)
-        if err != nil {
-            return err
-        }
-    }
+	// Creating folder path if it dosn't exist
+	_, err := os.Stat(getFolderPath(fileDest))
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(getFolderPath(fileDest), os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
 
-    return nil
+	return nil
 }
 
-func copyFile(fileDest, orgLocation string) error{
-    fmt.Printf("Copying: \"%s\" to \"%s\"\n", orgLocation, fileDest)
+func copyFile(fileDest, orgLocation string) error {
+	fmt.Printf("Copying: \"%s\" to \"%s\"\n", orgLocation, fileDest)
 
-    // Checks to see if the file already exists 
-    _, err := os.Stat(fileDest)
-    if os.IsNotExist(err) {
-         // Open the original file
-        original, err := os.Open(orgLocation)
-        if err != nil {
-            return err
-        }
-        defer original.Close()
+	// Checks to see if the file already exists
+	_, err := os.Stat(fileDest)
+	if os.IsNotExist(err) {
+		// Open the original file
+		original, err := os.Open(orgLocation)
+		if err != nil {
+			return err
+		}
+		defer original.Close()
 
-           new, err := os.Create(fileDest)
-        if err != nil {
-            return err
-        }
-        defer new.Close()
+		new, err := os.Create(fileDest)
+		if err != nil {
+			return err
+		}
+		defer new.Close()
 
-        bytesWritten, err := io.Copy(new, original)
-        if err != nil {
-            return err
-        }
+		bytesWritten, err := io.Copy(new, original)
+		if err != nil {
+			return err
+		}
 
-        fmt.Printf("Bytes Written: %d\n", bytesWritten)
-        return nil
-    }
+		fmt.Printf("Bytes Written: %d\n", bytesWritten)
+		return nil
+	}
 
-    fmt.Println("File already exists")
-    return nil
+	fmt.Println("File already exists")
+	return nil
 }
 
 func fileDest(orgLocation string) string {
-    stringToRemove := "/Users/dennis/Music/iTunes/iTunes Media/Music"
-    destination := "/Users/dennis/Desktop/test"
+	stringToRemove := "/Users/dennis/Music/iTunes/iTunes Media/Music"
+	destination := "/Volumes/Hugin/Music"
 
-    return destination + orgLocation[len(stringToRemove): ]
+	return destination + orgLocation[len(stringToRemove):]
 }
 
 // This removes the name of the music file from the string
 // This is to be able to create the folders before copying the file
 func getFolderPath(path string) string {
-    trailingSlash := 0
+	trailingSlash := 0
 
-    for i, letter := range path {
-        if string(letter) == "/" {                
-            trailingSlash= i
-        }
-    }
-    return path[0:trailingSlash]
+	for i, letter := range path {
+		if string(letter) == "/" {
+			trailingSlash = i
+		}
+	}
+	return path[0:trailingSlash]
 }
---
+++ main_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFileDest(t *testing.T) {
+	tests := []struct {
+		name  string
+		given string
+		want  string
+	}{
+		{
+			name:  "Artist / Album / Song",
+			given: "/Users/dennis/Music/iTunes/iTunes Media/Music/Alestorm/Back Through Time/01 Back Through Time.m4a",
+			want:  "/Volumes/Hugin/Music/Alestorm/Back Through Time/01 Back Through Time.m4a",
+		},
+		{
+			name:  "Folder",
+			given: "/Users/dennis/Music/iTunes/iTunes Media/Music/bob",
+			want:  "/Volumes/Hugin/Music/bob",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			val := fileDest(tt.given)
+
+			if val != tt.want {
+				t.Errorf("want %q; got %q", tt.want, val)
+			}
+		})
+	}
+}
+
+func TestGetFolderPath(t *testing.T) {
+	tests := []struct {
+		name  string
+		given string
+		want  string
+	}{
+		{
+			name:  "Long path",
+			given: "/Users/dennis/Music/iTunes/iTunes Media/Music/Alestorm/Back Through Time/01 Back Through Time.m4a",
+			want:  "/Users/dennis/Music/iTunes/iTunes Media/Music/Alestorm/Back Through Time",
+		},
+				{
+			name:  "Short path",
+			given: "/dennis/bob",
+			want:  "/dennis",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			val := getFolderPath(tt.given)
+
+			if val != tt.want {
+				t.Errorf("want %q; got %q", tt.want, val)
+			}
+		})
+	}
+}
